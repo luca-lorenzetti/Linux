b510a12171a923f559b299a553adba0d2f84dbcc
luca-lorenzetti <77396718+luca-lorenzetti@users.noreply.github.com>
2025-03-31 21:35:20 +0200

    Update myDistro.sh

--- MyDistro/myDistro.sh
+++ MyDistro/myDistro.sh
@@ -11,9 +11,19 @@ cd archiso-build
 # Copia del profilo di esempio
 cp -r /usr/share/archiso/configs/$PROFILE .
 
-# Personalizzazione del profilo (esempio)
-echo "vim" >> $PROFILE/packages.x86_64
-echo "tmux" >> $PROFILE/packages.x86_64
+# Elenco dei pacchetti installati
+pacman -Qqe > installed_packages.txt
+
+# Aggiunta dei pacchetti a packages.x86_64
+cat installed_packages.txt >> $PROFILE/packages.x86_64
+
+# Copia dei file di configurazione (esempio)
+sudo cp /etc/locale.gen $PROFILE/airootfs/etc/
+sudo cp /etc/locale.conf $PROFILE/airootfs/etc/
+
+# Personalizzazione di profiledef.sh (esempio)
+echo "loadkeys it" >> $PROFILE/profiledef.sh
+echo "setfont ter-132n" >> $PROFILE/profiledef.sh
 
 # Creazione dell'immagine ISO
 sudo mkarchiso -v -w . -o $ISO_NAME $PROFILE
